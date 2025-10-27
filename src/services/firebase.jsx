daf00eb6710e46c0c01d839fd05fcd7ba9aada7b
Santiago <alvarezsantiagonicolas105@hotmail.com>
2025-10-27 09:56:14 -0300

    agregamos firebase

---
+++ src/services/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDSwmIxWKNWTvt-FcKB1Sg-Xu4xKgN3RrA",
+  authDomain: "coder-comision-87990.firebaseapp.com",
+  projectId: "coder-comision-87990",
+  storageBucket: "coder-comision-87990.firebasestorage.app",
+  messagingSenderId: "173177924929",
+  appId: "1:173177924929:web:79bc61edac0c1b94e745c0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
