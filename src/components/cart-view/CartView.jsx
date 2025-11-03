12a503837477a5eb1ea4a6be0502ce75e00bcc78
Fenix1911 <alvarezsantiagonicolas105@hotmail.com>
2025-11-03 12:44:57 -0300

    1

--- src/components/cart-view/CartView.jsx
+++ src/components/cart-view/CartView.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import { CartContext } from '../../context/CartContext'
 import { Link } from 'react-router-dom'
-import '../../styles/cartView.css'
+import '../../styles/CartView.css'
 
 
 
